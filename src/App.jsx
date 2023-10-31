e618b1a4eec6f684bb11e89cbe4d144d1d121cfb
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-10-31 08:28:21 -0400

    Add state for moles

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function Mole() {
   return <div>M</div>;
 }
@@ -7,18 +9,14 @@ function Hole() {
 }
 
 export default function App() {
+  const [moles, setMoles] = useState(new Array(9).fill(false));
+
   return (
     <>
       <div className="grid grid-cols-3 mx-auto w-1/2 border mt-6">
-        <Hole />
-        <Hole />
-        <Hole />
-        <Hole />
-        <Hole />
-        <Hole />
-        <Hole />
-        <Hole />
-        <Hole />
+        {moles.map((isMole) => {
+          isMole ? <Mole /> : <Hole />;
+        })}
       </div>
     </>
   );
