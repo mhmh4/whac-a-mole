d12c2502b073438488228addccc94c8b5d5abd8a
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-10-31 08:25:20 -0400

    Create grid of holes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,25 @@
+function Mole() {
+  return <div>M</div>;
+}
+
+function Hole() {
+  return <div>H</div>;
+}
+
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <div className="grid grid-cols-3 mx-auto w-1/2 border mt-6">
+        <Hole />
+        <Hole />
+        <Hole />
+        <Hole />
+        <Hole />
+        <Hole />
+        <Hole />
+        <Hole />
+        <Hole />
+      </div>
+    </>
+  );
 }
